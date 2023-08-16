aec0366df6818b70d38d271b7d7c64de2602944e
krisrok <3404365+krisrok@users.noreply.github.com>
2023-08-16 15:38:38 +0200

    Add Layer attribute and LayerMask extensions

---
+++ EditorAddons/Runtime/LayerMaskUtils.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public static class LayerMaskExtensions
+{
+    /// <summary>
+    /// Extension method to check if a layer is in a layermask
+    /// </summary>
+    /// <param name="mask"></param>
+    /// <param name="layer"></param>
+    /// <returns></returns>
+    public static bool Contains(this LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+
+    /// <summary> Converts given mask to layer number </summary>
+    /// <returns> layer number </returns>
+    public static int ToSingleLayerNumber(this LayerMask mask)
+    {
+        int result = mask > 0 ? 0 : 31;
+        while (mask > 1)
+        {
+            mask = mask >> 1;
+            result++;
+        }
+        return result;
+    }
+}
---
+++ Odin Addons/Runtime/Attributes/LayerAttributePropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace OdinAddons
+{
+    /// <summary>
+    /// Attribute to select a single layer.
+    /// </summary>
+    public class LayerAttribute : PropertyAttribute
+    { }
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    class LayerAttributePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+            EditorGUI.EndProperty();
+        }
+    }
+#endif
+}
