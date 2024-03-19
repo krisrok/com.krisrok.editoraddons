fab27c95f3f667b33537285bb579dd164c40758e
krisrok <3404365+krisrok@users.noreply.github.com>
2024-03-19 15:17:59 +0100

    Add tool window to locate hidden root GameObjects

---
+++ OdinAddons/Editor/FindHiddenObjectsWindow.cs
@@ -0,0 +1,62 @@
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace OdinAddons.Editor
+{
+    public class FindHiddenObjectsWindow : OdinEditorWindow
+    {
+
+        [MenuItem("Tools/Find hidden GameObjects")]
+        private static void Open()
+        {
+            var window = EditorWindow.GetWindow<FindHiddenObjectsWindow>();
+            window.Show();
+        }
+
+        [Serializable]
+        internal class HiddenObject
+        {
+            public GameObject GameObject;
+
+            [OnValueChanged(nameof(ValueChanged))]
+            public HideFlags HideFlags;
+
+            private void ValueChanged()
+            {
+                GameObject.hideFlags = HideFlags;
+                EditorUtility.SetDirty(GameObject);
+            }
+        }
+
+        [SerializeField]
+        private List<HiddenObject> _hiddenObjects;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            UpdateList();
+        }
+
+        [Button]
+        private void UpdateList()
+        {
+            _hiddenObjects = EditorSceneManager.GetActiveScene().GetRootGameObjects()
+                .Where(ro => ro.hideFlags != HideFlags.None)
+                .Select(ro => new HiddenObject { GameObject = ro, HideFlags = ro.hideFlags })
+                .ToList();
+        }
+
+        protected override void OnGUI()
+        {
+            EditorGUILayout.HelpBox(new GUIContent("Searches for hidden root level GameObjects"));
+
+            base.OnGUI();
+        }
+    }
+}
