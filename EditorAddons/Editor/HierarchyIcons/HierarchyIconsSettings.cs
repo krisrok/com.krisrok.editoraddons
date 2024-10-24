6a854e4a9ca0a38d54edebc93bb6d22a81e00b10
krisrok <3404365+krisrok@users.noreply.github.com>
2024-10-24 14:24:46 +0200

    Minor cleanup

--- EditorAddons/Editor/HierarchyIcons/HierarchyIconsSettings.cs
+++ EditorAddons/Editor/HierarchyIcons/HierarchyIconsSettings.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 using SerializableSettings;
 using SerializableSettings.Editor;
 
-[EditorUserSettings("Hierarchy Icons ⁂")]
-public class HierarchyIconsSettings : SerializableSettings<HierarchyIconsSettings>
+namespace EditorAddons.Editor
 {
-    // Settings defined here can be accessed during runtime and in the Unity Editor.
-    // Make sure to keep the #if UNITY_EDITOR directives or put the script into an Editor-only assembly.
-    // Access the settings via Edit/Preferences/... and via code: HierarchyIconsSettings.Instance.
-    // These settings can be different on a per-user (or per-developer) basis. Think favourite editor color.
-    // Please exclude the Assets/Settings/Editor/User/ folder in e.g. your .gitignore file.
+    [EditorUserSettings("Editor Addons ⁂/Hierarchy Icons")]
+    public class HierarchyIconsSettings : SerializableSettings<HierarchyIconsSettings>
+    {
+        // Settings defined here can be accessed during runtime and in the Unity Editor.
+        // Make sure to keep the #if UNITY_EDITOR directives or put the script into an Editor-only assembly.
+        // Access the settings via Edit/Preferences/... and via code: HierarchyIconsSettings.Instance.
+        // These settings can be different on a per-user (or per-developer) basis. Think favourite editor color.
+        // Please exclude the Assets/Settings/Editor/User/ folder in e.g. your .gitignore file.
 #else
 public class HierarchyIconsSettings : ScriptableObject
 {
@@ -19,11 +21,12 @@ public class HierarchyIconsSettings : ScriptableObject
     public static HierarchyIconsSettings Instance => _instance ??= CreateInstance<HierarchyIconsSettings>();
 #endif
 
-    [field: Header("Default drawers")]
-    [field: SerializeField]
-    public bool DrawObjectIcon { get; private set; }
+        [field: Header("Default drawers")]
+        [field: SerializeField]
+        public bool DrawObjectIcon { get; private set; }
 
-    [field: SerializeField]
-    public bool DrawActiveToggle { get; private set; } = true;
+        [field: SerializeField]
+        public bool DrawActiveToggle { get; private set; } = true;
+    }
 }
 #endif
